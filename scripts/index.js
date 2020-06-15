72ff7874e20054e925bbaa05ec0246f566cb3ca6
Roman Didukh <newrdlink@yandex.ru>
2020-06-15 16:24:46 +0300

    подключить шрифты

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let editForm = document.querySelector(".edit-form");
+const editForm = document.querySelector(".edit-form");
 const editFormOpen = document.querySelector(".profile__edit");
 const editFormClose = editForm.querySelector(".edit-form__button-close");
 const editFormSaveClose = editForm.querySelector(".edit-form__button");
@@ -8,8 +8,6 @@ const editFormToggle = function (event) {
 };
 
 const editFormCloseByOverlay = function (event) {
-  console.log(event.target);
-  console.log(event.currentTarget);
   if (event.target !== event.currentTarget) {
     return;
   }
@@ -19,15 +17,18 @@ const editFormCloseByOverlay = function (event) {
 editFormOpen.addEventListener("click", editFormToggle);
 editFormClose.addEventListener("click", editFormToggle);
 editForm.addEventListener("click", editFormCloseByOverlay);
-//
+
 const nameInput = editForm.querySelector(".edit-form__name");
 const jobInput = editForm.querySelector(".edit-form__about");
 
 let name = nameInput.getAttribute("value");
 let job = jobInput.getAttribute("value");
 
-let nameTarget = document.querySelector(".profile__title");
-let jobTarget = document.querySelector(".profile__subtitle");
+nameInput.value = "Жак-Ив Кусто";
+jobInput.value = "Исследователь океана";
+
+const nameTarget = document.querySelector(".profile__title");
+const jobTarget = document.querySelector(".profile__subtitle");
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
