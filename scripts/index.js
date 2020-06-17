e773ab75861652157c7582713365b842e03f834e
Roman Didukh <newrdlink@yandex.ru>
2020-06-17 13:14:15 +0300

    исправить замечания после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,28 @@
 const editForm = document.querySelector(".edit-form");
 const editFormOpen = document.querySelector(".profile__edit");
 const editFormClose = editForm.querySelector(".edit-form__button-close");
+
 const editFormSaveClose = editForm.querySelector(".edit-form__button");
 
-const editFormToggle = function (event) {
-  editForm.classList.toggle("edit-form_opened");
-};
-
-const editFormCloseByOverlay = function (event) {
-  if (event.target !== event.currentTarget) {
-    return;
-  }
-  editFormToggle();
-};
-
-editFormOpen.addEventListener("click", editFormToggle);
-editFormClose.addEventListener("click", editFormToggle);
-editForm.addEventListener("click", editFormCloseByOverlay);
-
-const nameInput = editForm.querySelector(".edit-form__name");
-const jobInput = editForm.querySelector(".edit-form__about");
-
-let name = nameInput.getAttribute("value");
-let job = jobInput.getAttribute("value");
-
-nameInput.value = "Жак-Ив Кусто";
-jobInput.value = "Исследователь океана";
-
 const nameTarget = document.querySelector(".profile__title");
-const jobTarget = document.querySelector(".profile__subtitle");
+const aboutTarget = document.querySelector(".profile__subtitle");
+
+const nameInput = editForm.querySelector(".edit-form__item_name");
+const aboutInput = editForm.querySelector(".edit-form__item_about");
+
+function closePopUp() {
+  editForm.classList.toggle("edit-form_opened");
+  nameInput.value = nameTarget.textContent;
+  aboutInput.value = aboutTarget.textContent;
+}
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
   nameTarget.textContent = nameInput.value;
-  jobTarget.textContent = jobInput.value;
+  aboutTarget.textContent = aboutInput.value;
+  closePopUp();
 }
 
+editFormOpen.addEventListener("click", closePopUp);
+editFormClose.addEventListener("click", closePopUp);
 editForm.addEventListener("submit", formSubmitHandler);
-
-editFormSaveClose.addEventListener("click", editFormToggle);
