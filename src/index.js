9be6afa5438e9425fa7f9c8cdc42739030645040
Roman Didukh <newrdlink@yandex.ru>
2020-08-24 10:47:04 +0300

    after first review

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//import "../styles/index.css";
+import "../styles/index.css";
 import { allElements } from "../scripts/elements.js";
 import Card from "../components/Card.js";
 import Section from "../components/Section.js";
